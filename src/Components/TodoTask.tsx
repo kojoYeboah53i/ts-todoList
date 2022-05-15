0b53d3d8c6f3088cd32875e88f95111049cc2128
Isaac Kojo Yeboah <yeboah.determined.isaac@gmail.com>
2022-05-15 11:19:20 +0000

    up

---
+++ src/Components/TodoTask.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ITask } from '../interface';
+
+interface Props {
+
+    task: ITask
+    completeTask(taskNameToDelete: string): void;
+  }
+
+const TodoTask = ({task, completeTask}: Props) => {
+    
+  return (
+    <div className="task">
+    <div className="content">
+      <span>{task.taskName}</span>
+      <span>{task.deadLine}</span>
+    </div>
+    <button
+      onClick={() => {
+        completeTask(task.taskName);
+      }}
+    >
+      X
+    </button>
+  </div>
+  );
+}
+
+
+
+export default TodoTask;
